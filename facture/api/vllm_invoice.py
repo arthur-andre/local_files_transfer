65dfbff5b4aa5da796761adf6494b3a758617832
arthur-andre <82572124+arthur-andre@users.noreply.github.com>
2025-06-30 12:05:28 +0200

    float

--- facture/api/vllm_invoice.py
+++ facture/api/vllm_invoice.py
@@ -107,7 +107,7 @@ def filtrer_reponse_json(reponse):
                 
 
                 print(f"Montant TTC formaté : {data.get('montant_TTC')}, Montant TVA formaté : {data.get('montant_TVA')}")
-                print('soustraction TTC - TVA :', montant_TTC - montant_TVA)
+                print('soustraction TTC - TVA :', float(montant_TTC) - float(montant_TVA))
                 print(f"Séparateur utilisé : {separateur}")
                 if montant_TTC is not None and montant_TVA is not None:
                     montant_HT = montant_TTC - montant_TVA
